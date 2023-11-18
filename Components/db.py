b0c0e1f73efc7f161ab5db2d81d0bd246fc1b89b
Miguel <mvarvalenc26@gmail.com>
2023-11-18 18:47:32 -0500

    Corrección de la última versión

--- Components/db.py
+++ Components/db.py
@@ -13,13 +13,13 @@ def create_db(db_name="tiendas.db",db_path='DB'):
         # Creation of tables
 
         cursor.execute("""CREATE TABLE IF NOT EXISTS `tbCliente` (
-            `num_documento_cliente` INTEGER NOT NULL,
+            `num_documento_cliente` TEXT NOT NULL,
             `tipo_documento_cliente` TEXT NOT NULL,
             PRIMARY KEY (`num_documento_cliente`)
             );""")
         
         cursor.execute("""CREATE TABLE IF NOT EXISTS `tbBarrio` (
-            `id_barrio` INTEGER NOT NULL,
+            `id_barrio` TEXT NOT NULL,
             `nombre_barrio` TEXT NOT NULL,
             PRIMARY KEY (`id_barrio`)
             );""")
@@ -31,16 +31,16 @@ def create_db(db_name="tiendas.db",db_path='DB'):
 
         cursor.execute("""CREATE TABLE IF NOT EXISTS `tbTienda` (
             `codigo_tienda` INTEGER NOT NULL,
-            `total_compra` TEXT NOT NULL,
+            `total_compra` REAL NOT NULL,
             `tbTipoTienda_id_TipoTienda` INTEGER NOT NULL,
-            `tbBarrio_id_barrio` INTEGER NOT NULL,
+            `tbBarrio_id_barrio` TEXT NOT NULL,
             PRIMARY KEY (`codigo_tienda`, `tbTipoTienda_id_TipoTienda`, `tbBarrio_id_barrio`),
             FOREIGN KEY (`tbTipoTienda_id_TipoTienda`) REFERENCES `tbTipoTienda` (`id_TipoTienda`),
             FOREIGN KEY (`tbBarrio_id_barrio`) REFERENCES `tbBarrio` (`id_barrio`)
             );""")
         
         cursor.execute("""CREATE TABLE IF NOT EXISTS `tbCliente_has_tbTienda` (
-            `tbCliente_num_documento_cliente` INTEGER NOT NULL,
+            `tbCliente_num_documento_cliente` TEXT NOT NULL,
             `tbTienda_codigo_tienda` INTEGER NOT NULL,
             PRIMARY KEY (`tbCliente_num_documento_cliente`, `tbTienda_codigo_tienda`),
             FOREIGN KEY (`tbCliente_num_documento_cliente`) REFERENCES `tbCliente` (`num_documento_cliente`),
